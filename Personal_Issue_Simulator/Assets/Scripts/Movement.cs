c322a2eec2e27d91873802c96ef602c617742346
SunrayF <Sunray-muijsson@hotmail.com>
2017-04-08 14:28:22 +0200

    project toegevoegd

---
+++ Personal_Issue_Simulator/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public bool sprinting;
+	private float x;
+	private float z;
+	public float speed;
+	public float runSpeed;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		Move ();
+
+	}
+
+	void Move (){
+
+		x = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+		z = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+
+		transform.Translate(x, 0, z);
+		print (x);
+
+	}
+
+	//void OnCollisionEnter (Collision col){
+		
+		//print ("hit");
+	//}
+}
