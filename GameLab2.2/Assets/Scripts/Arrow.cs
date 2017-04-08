f22252dd12b98be41b9074bf97ef37b22ec033e3
SunrayF <Sunray-muijsson@hotmail.com>
2017-04-08 14:59:09 +0200

    Goede project toegvoegd

---
+++ GameLab2.2/Assets/Scripts/Arrow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	public GameObject arrow;
+	public GameObject bow;
+	public float speed;
+	public float speedAdd;
+	private float timer;
+	public float upgradePowerSpeed;
+	private bool released;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		HoldArrow ();
+		DrawingBow ();
+	}
+
+	void FixedUpdate(){
+
+		if(Input.GetMouseButtonUp(0)){
+			released = true;
+			gameObject.AddComponent<Rigidbody>();
+			arrow.transform.position = transform.position + Camera.main.transform.forward * 2;
+			Rigidbody rb = arrow.GetComponent<Rigidbody>();
+			rb.velocity = Camera.main.transform.forward * speed;
+		}
+
+	}
+
+	void HoldArrow(){
+		
+		if(released == false){
+			print ("holding");
+		}
+	}
+
+	void DrawingBow(){
+
+		if (Input.GetMouseButton (0)) {
+			timer += Time.deltaTime;
+			if (timer > upgradePowerSpeed) {
+				speed += speedAdd;
+				timer = 0;
+			}
+			print (speed);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Destroy (gameObject);
+	}
+}
